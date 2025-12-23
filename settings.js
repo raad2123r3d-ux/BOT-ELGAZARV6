339e08727ff913d6e9e9942f34f7038674d389dd
raad2123r3d-ux <raad2123r3d@gmail.com>
2025-12-23 20:54:04 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,16 +22,16 @@ global.socialm = "GitHub: ELGAZAR" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.ownernomer = "212625834328" //ur number
+global.premium = ['212625834328'] //ur premium number
+global.botname = '❤️anos ᗷOT❤️' //ur bot name
+global.linkz = "" //your theme url which will be displayed on whatsapp
+global.websitex = "" //ur website to be displayed
+global.botscript = '' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
+global.packname = "انوس عمك⍣" //ur sticker watermark packname
 global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.wm = "❤️anos ᗷOT❤️." //ur watermark
 
 // Other
 global.sessionName = 'session'
@@ -128,3 +128,4 @@ fs.watchFile(file, () => {
 	delete require.cache[file]
 	require(file)
 })
+
